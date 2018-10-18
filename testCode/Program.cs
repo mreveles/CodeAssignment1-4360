6d714b29df13580787679d3ea3e7dffd8d3974c4
mreveles <mareveles1@buffs.wtamu.edu>
2018-10-18 14:08:04 -0500

    First Commint

---
+++ testCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace testCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Employee 
+    {
+        string eName;
+        int age;
+        float salary;
+        public Employee(string eName, float salary, int age)
+        {
+            this.eName = new string(eName);
+            this.salary = salary;
+            this.age = age;
+        }
+        public void displayEmpInfo(){
+            Console.WriteLine("Empl. Name = {0} \n Empl Age = {1} \n Empl Salary = {2}", eName, age, salary);
+        }
+    }
+}
